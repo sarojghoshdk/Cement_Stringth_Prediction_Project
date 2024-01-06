45f9aab47e4134e728c500766d94e26d0b32b1bd
Saroj Ghosh <sarojghoshdk@gmail.com>
2024-01-06 13:41:34 +0530

    first commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+MONGO_DATABASE_NAME = "cement_stringth_project"
+MONGO_COLLECTION_NAME = "cementstringth"
+AWS_S3_BUCKET_NAME = "sensor-deployment"
+
+
+
+TARGET_COLUMN = "cement_strength"
+MONGO_DB_URL= "mongodb+srv://sarojghoshdk:sarojghoshdk@cluster0.z2glfmr.mongodb.net/?retryWrites=true&w=majority"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
